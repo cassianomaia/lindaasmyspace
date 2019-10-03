35b740a59a7275b89c093d9ad7502d7e9047153a
Cassiano <cassmala@gmail.com>
2019-10-03 16:19:17 -0300

    Get funcional.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, json
 from flask_restful import Api, Resource, reqparse
 import linda
 
@@ -15,7 +15,16 @@ class Blog(Resource):
         parser.add_argument("topico")
         args = parser.parse_args()
         message = blog._rd((user,args["topico"],str))
-        return message,200
+        messages = message.splitlines()
+        if len(messages) > 0:
+            response = app.response_class(
+                response=json.dumps(messages),
+                status=200,
+                mimetype='application/json'
+            )
+            return response
+        else:
+            return 404
 
     def post(self, user):
         parser = reqparse.RequestParser()
